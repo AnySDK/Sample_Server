2f0660e28520cef1fd4917cde34b8b2a87363ead
libo <libo@chukong-inc.com>
2016-05-09 09:59:52 +0800

    完善服务端接入demo，新增广告追踪sdk的接入

--- php/AnySDK.config.php
+++ php/AnySDK.config.php
@@ -2,16 +2,25 @@
 /**
  * AnySDK服务端SDK config for PHP
  * 
- * @author    李播<libo@anysdk.com>
+ * @author      李播<libo@anysdk.com>
  * @date        2016-01-11
- * @version  2.0.0     
- * @link         http://docs.anysdk.com/OauthLogin         统一登录验证
- * @link         http://docs.anysdk.com/PaymentNotice  订单支付通知
+ * @version     2.0.0
+ * 
+ * @author      李播<libo@anysdk.com>
+ * @date        2016-04-14
+ * @version     2.1.0
+ * 
+ * @link        http://docs.anysdk.com/OauthLogin     统一登录验证
+ * @link        http://docs.anysdk.com/PaymentNotice  订单支付通知
+ * @link        http://docs.anysdk.com/AdTracking     广告效果追踪
  */
-defined('LOGIN_CHECK_URL') or define('LOGIN_CHECK_URL', 'http://oauth.anysdk.com/api/User/LoginOauth/');
-defined('DEBUG_MODE') or define('DEBUG_MODE', FALSE);
+defined('LOGIN_CHECK_URL')        or define('LOGIN_CHECK_URL',          'http://oauth.anysdk.com/api/User/LoginOauth/');
+defined('ADTRACKING_REPORT_URL')  or define('ADTRACKING_REPORT_URL',    'http://pay.anysdk.com/v5/AdTracking/Submit/');
+defined('DEBUG_MODE')             or define('DEBUG_MODE',               FALSE);
 
-// 增强密钥        前往dev.anysdk.com => 游戏列表 获取，此参数请严格保密
-defined('ANYSDK_ENHANCED_KEY') or define('ANYSDK_ENHANCED_KEY', 'YTdkY2Q5ZmRmNDUxMjkxYzAxOTM');
+// 游戏ID              前往dev.anysdk.com => 游戏列表 获取
+defined('ANYSDK_GAME_ID')         or define('ANYSDK_GAME_ID',           0);
+// 增强密钥             前往dev.anysdk.com => 游戏列表 获取，此参数请严格保密
+defined('ANYSDK_ENHANCED_KEY')    or define('ANYSDK_ENHANCED_KEY',      '');
 // private_key        前往dev.anysdk.com => 游戏列表 获取
-defined('ANYSDK_PRIVATE_KEY') or define('ANYSDK_PRIVATE_KEY', 'CB2B5A269635B23ACD4FBE1D7B687274');
+defined('ANYSDK_PRIVATE_KEY')     or define('ANYSDK_PRIVATE_KEY',       '');
---
+++ php/READMD.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 2.1.0
+
+2016年04月14日 SDK增加广告追踪功能，并增加demo代码
+
+## 2.0.0
+
+2016年01月11日 将服务端PHP Sample重构为AnySDK服务端SDK for PHP
